1ae632f00befd7133eebcc996a2aad5efce48ab0
Madhvi <madhvi.saurabhinfosys@gmail.com>
2023-02-03 14:15:30 +0530

    Initial commit

---
+++ lib/utils/my_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static Color white = Colors.white;
+  static Color black = Colors.black;
+}
---
+++ lib/utils/my_images.dart
@@ -0,0 +1,13 @@
+class MyImages {
+  static String baseUrl = "assets/images";
+
+  static String logo = "$baseUrl/logo.png";
+  static String about = "$baseUrl/about.png";
+  static String addCard = "$baseUrl/add_card.png";
+  static String apple = "$baseUrl/apple.png";
+  static String automateMsg = "$baseUrl/automate_msg.png";
+  static String backArrow = "$baseUrl/back_arrow.png";
+  static String backArrowBorder = "$baseUrl/back_arrow_border.png";
+  static String businessAndTrade = "$baseUrl/business_and_trade.png";
+  static String business = "$baseUrl/business.png";
+}
