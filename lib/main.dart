e3d62238a67d4eb3f5e231568a6c22f7b004904a
khushalisatani <khushali1.saurabhinfosys@gmail.com>
2023-04-20 18:44:05 +0530

    CHANGES test.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:insta_job/bloc/bloc_providers.dart';
 import 'package:insta_job/globals.dart';
+import 'package:insta_job/screens/insta_recruit/splash_screen.dart';
 import 'package:insta_job/screens/insta_recruit/splash_screen_job/test.dart';
 
 import 'di_container.dart' as di;
