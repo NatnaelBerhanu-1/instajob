8a4ac651e83b659e1706f9d55b2dbd4418320152
dana1 <danielaraya2191@gmail.com>
2024-04-19 15:47:12 +0300

    wip: fixes on pick interview time slot screen

--- lib/screens/pick_interview_time_slot_screen/pick_interview_time_slot_screen.dart
+++ lib/screens/pick_interview_time_slot_screen/pick_interview_time_slot_screen.dart
@@ -1,9 +1,11 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart';
+import 'package:intl/intl.dart';
+
+import 'package:insta_job/utils/my_colors.dart';
 import 'package:insta_job/utils/my_images.dart';
 import 'package:insta_job/widgets/custom_app_bar.dart';
-import 'package:intl/intl.dart';
-// import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 
 class BookSlotScreen extends StatefulWidget {
   const BookSlotScreen({super.key});
@@ -14,11 +16,11 @@ class BookSlotScreen extends StatefulWidget {
 
 class _BookSlotScreenState extends State<BookSlotScreen> {
   DateTime selectedDate = DateTime.now();
+  TimeSlot? selectedTimeSlot;
 
   void _pickDate() {
     DatePicker.showDatePicker(context,
         showTitleActions: true,
-        // minTime: DateTime(2000, 1, 1),
         minTime: DateTime.now(),
         maxTime: DateTime(2101, 12, 31),
         currentTime: selectedDate, onConfirm: (date) {
@@ -32,8 +34,8 @@ class _BookSlotScreenState extends State<BookSlotScreen> {
     });
   }
 
-  List<Widget> _generateTimeSlots(DateTime startTime, DateTime endTime) {
-    List<Widget> slots = [];
+  List<TimeSlot> _generateTimeSlots(DateTime startTime, DateTime endTime) {
+    List<TimeSlot> slots = [];
     DateTime currentDateTime = selectedDate;
 
     DateTime counterRunningTime = startTime;
@@ -46,23 +48,13 @@ class _BookSlotScreenState extends State<BookSlotScreen> {
         counterRunningTime.isAtSameMomentAs(endTime))) {
       if (isSameDateAsToday(currentDateTime) &&
           counterRunningTime.isBefore(currentDateTime)) {
-        counterRunningTime = counterRunningTime.add(Duration(minutes: 10));
+        counterRunningTime =
+            counterRunningTime.add(const Duration(minutes: 10));
         continue;
       }
-      String timeSuffix = currentDateTime.hour < 12 ? 'am' : 'pm';
+      var slotItem = TimeSlot(time: counterRunningTime);
       slots.add(
-        Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: ActionChip(
-            label: Text(
-                "${counterRunningTime.hour}:${counterRunningTime.minute.toString().padLeft(2, '0')} $timeSuffix"),
-            onPressed: () {
-              // Handle time slot selection here
-              print(
-                  "Selected time slot: ${counterRunningTime.hour}:${counterRunningTime.minute}");
-            },
-          ),
-        ),
+        slotItem,
       );
       counterRunningTime = counterRunningTime.add(const Duration(minutes: 10));
     }
@@ -83,10 +75,10 @@ class _BookSlotScreenState extends State<BookSlotScreen> {
 
     final dateFormat = DateFormat('EEEE, dd MMMM yyyy');
 
-    List<Widget> morningSlots =
+    List<TimeSlot> morningSlots =
         _generateTimeSlots(morningStartTime, morningEndTime);
 
-    List<Widget> afternoonSlots =
+    List<TimeSlot> afternoonSlots =
         _generateTimeSlots(afternoonStartTime, afternoonEndTime);
 
     return Scaffold(
@@ -102,17 +94,19 @@ class _BookSlotScreenState extends State<BookSlotScreen> {
         ),
       ),
       body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
+        // physics: BouncingScrollPhysics(), //revisit
         child: Column(
           children: [
             Container(
-              // color: Colors.red,
               padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   IconButton(
-                    icon: const Icon(Icons.arrow_back_ios),
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      size: 20,
+                    ),
                     onPressed: () {
                       setState(() {
                         selectedDate =
@@ -129,7 +123,10 @@ class _BookSlotScreenState extends State<BookSlotScreen> {
                     ),
                   ),
                   IconButton(
-                    icon: const Icon(Icons.arrow_forward_ios),
+                    icon: const Icon(
+                      Icons.arrow_forward_ios,
+                      size: 20,
+                    ),
                     onPressed: () {
                       setState(() {
                         selectedDate =
@@ -142,56 +139,23 @@ class _BookSlotScreenState extends State<BookSlotScreen> {
             ),
 
             // Morning Slots Section
-            _buildMorningSlotSection(morningSlots),
+            _buildSlotSection(slotTitle: "Morning", slots: morningSlots),
 
             const SizedBox(height: 20),
             // Afternoon Slots Section
-            _buildAfternoonSlotSection(afternoonSlots),
+            _buildSlotSection(slotTitle: "Afternoon", slots: afternoonSlots),
+            // _buildAfternoonSlotSection(afternoonSlots),
           ],
         ),
       ),
     );
   }
 
-  Padding _buildAfternoonSlotSection(List<Widget> afternoonSlots) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            'Afternoon',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            // child: Wrap(
-            //   children:
-            //       _generateTimeSlots(morningStartTime, morningEndTime),
-            // ),
-            child: afternoonSlots.isEmpty
-                ? Text("No data")
-                : GridView.builder(
-                    shrinkWrap: true,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 5, // Number of columns
-                      crossAxisSpacing: 8.0,
-                      mainAxisSpacing: 0.0,
-                    ),
-                    itemCount: afternoonSlots.length,
-                    itemBuilder: (context, index) {
-                      return afternoonSlots[index];
-                    },
-                  ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Padding _buildMorningSlotSection(List<Widget> morningSlots) {
-    return Padding(
+  Widget _buildSlotSection({
+    required String slotTitle,
+    required List<TimeSlot> slots,
+  }) {
+    return Container(
       padding: const EdgeInsets.symmetric(horizontal: 8.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -200,21 +164,36 @@ class _BookSlotScreenState extends State<BookSlotScreen> {
             'Morning',
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
-          Padding(
+          const SizedBox(height: 16),
+          Container(
             padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: morningSlots.isEmpty
+            child: slots.isEmpty
                 ? const Text("No data")
                 : GridView.builder(
                     shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
                     gridDelegate:
                         const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 5, // Number of columns
-                      crossAxisSpacing: 8.0,
-                      mainAxisSpacing: 0.0,
+                      crossAxisCount: 4, // Number of columns
+                      // crossAxisSpacing: 0.0,
+                      // mainAxisSpacing: 0.0,
+                      childAspectRatio:
+                          2.2, // Adjust this value to control the aspect ratio (width/height) of each grid item
                     ),
-                    itemCount: morningSlots.length,
+                    itemCount: slots.length,
                     itemBuilder: (context, index) {
-                      return morningSlots[index];
+                      var item = slots[index];
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedTimeSlot = item;
+                          });
+                        },
+                        child: TimeSlotChip(
+                          isSelected: selectedTimeSlot == item,
+                          timeSlot: item,
+                        ),
+                      );
                     },
                   ),
           ),
@@ -224,40 +203,45 @@ class _BookSlotScreenState extends State<BookSlotScreen> {
   }
 }
 
-class TimeSlotChip extends StatelessWidget {
-  final String timeText;
-  final bool isSelected;
-  final VoidCallback onTap;
+class TimeSlot extends Equatable {
+  final DateTime time;
 
+  const TimeSlot({required this.time});
+
+  @override
+  List<Object?> get props => [time];
+}
+
+class TimeSlotChip extends StatelessWidget {
+  final TimeSlot timeSlot;
+  final bool isSelected;
   const TimeSlotChip({
-    super.key,
-    required this.timeText,
+    Key? key,
+    required this.timeSlot,
     required this.isSelected,
-    required this.onTap,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final textColor =
+        isSelected ? MyColors.white : MyColors.black.withOpacity(0.5);
+    final backgroundColor = isSelected ? MyColors.blue : null;
+
+    DateTime currentTime = timeSlot.time;
+    DateFormat timeFormat = DateFormat('h:mm a');
+
     return Padding(
-      padding: const EdgeInsets.all(4.0),
-      child: GestureDetector(
-        onTap: onTap,
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
-          decoration: BoxDecoration(
-            color: isSelected ? Colors.blueAccent : Colors.grey[200],
-            borderRadius: BorderRadius.circular(8.0),
-            border: Border.all(
-              color: isSelected ? Colors.blue : Colors.grey,
-              width: 1.0,
-            ),
-          ),
-          child: Text(
-            timeText,
-            style: TextStyle(
-              color: isSelected ? Colors.white : Colors.black,
-              fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
-            ),
+      padding: const EdgeInsets.all(4).copyWith(bottom: 0),
+      child: Container(
+        padding: const EdgeInsets.all(4).copyWith(top: 2),
+        color: backgroundColor,
+        child: Text(
+          timeFormat.format(currentTime),
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 16,
+            color: textColor,
           ),
         ),
       ),
