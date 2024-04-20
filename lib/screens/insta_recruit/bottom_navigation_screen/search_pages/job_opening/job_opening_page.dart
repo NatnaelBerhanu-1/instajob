f4d268fc9f9d1f57b3e4574afa11f8706aae1511
dana1 <danielaraya2191@gmail.com>
2024-04-20 15:56:42 +0300

    job opening ui fixes 2

--- lib/screens/insta_recruit/bottom_navigation_screen/search_pages/job_opening/job_opening_page.dart
+++ lib/screens/insta_recruit/bottom_navigation_screen/search_pages/job_opening/job_opening_page.dart
@@ -49,189 +49,185 @@ class _JobOpeningScreenState extends State<JobOpeningScreen> {
         appBar: PreferredSize(
             preferredSize: Size(double.infinity, 90),
             child: SafeArea(
-              child: Row(
-                // elevation: 0,
-                // leadingWidth: 50,
-                // backgroundColor: MyColors.white,
-                // toolbarHeight: 70,
-                // title:
-                children: [
-                  ImageButton(
-                    image: MyImages.backArrowBorder,
-                    color: MyColors.blue,
-                    height: 28,
-                    width: 28,
-                    padding: EdgeInsets.all(9.0),
-                    onTap: () {
-                      context.read<CompanyBloc>().add(LoadCompanyListEvent());
-                      context.read<BottomBloc>().add(
-                          SetScreenEvent(false, screenName: AssignCompany()));
-                      // context.read<BottomCubit>().setSelectedScreen(false,
-                      //     screenName: JobOpeningScreen());
-                    },
-                  ),
-                  Global.userModel?.type == "user"
-                      ? Expanded(
-                          child: CommonText(
-                            text: " ${widget.companyModel?.companyName}",
-                            fontWeight: FontWeight.w600,
-                            fontSize: 18,
-                          ),
-                        )
-                      : Expanded(
-                          child: IconTextField(
-                            prefixIcon: ImageButton(
-                              image: MyImages.searchGrey,
-                              padding: EdgeInsets.all(14),
-                              height: 10,
-                              width: 10,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 6).copyWith(top: 28),
+                child: Row(
+                  // elevation: 0,
+                  // leadingWidth: 50,
+                  // backgroundColor: MyColors.white,
+                  // toolbarHeight: 70,
+                  // title:
+                  children: [
+                    ImageButton(
+                      image: MyImages.backArrowBorder,
+                      color: MyColors.blue,
+                      height: 28,
+                      width: 28,
+                      padding: EdgeInsets.all(9.0),
+                      onTap: () {
+                        context.read<CompanyBloc>().add(LoadCompanyListEvent());
+                        context.read<BottomBloc>().add(
+                            SetScreenEvent(false, screenName: AssignCompany()));
+                        // context.read<BottomCubit>().setSelectedScreen(false,
+                        //     screenName: JobOpeningScreen());
+                      },
+                    ),
+                    SizedBox(width: 5),
+                    Global.userModel?.type == "user"
+                        ? Expanded(
+                            child: CommonText(
+                              text: " ${widget.companyModel?.companyName}",
+                              fontWeight: FontWeight.w600,
+                              fontSize: 18,
                             ),
-                            readOnly: true,
-                            borderRadius: 25,
-                            hint: "search",
-                            onPressed: () {
-                              AppRoutes.push(
-                                  context, SearchCompany(isJobSearch: true));
-                            },
+                          )
+                        : Expanded(
+                            child: IconTextField(
+                              prefixIcon: ImageButton(
+                                image: MyImages.searchGrey,
+                                padding: EdgeInsets.all(14),
+                                height: 10,
+                                width: 10,
+                              ),
+                              readOnly: true,
+                              borderRadius: 25,
+                              hint: "search",
+                              onPressed: () {
+                                AppRoutes.push(
+                                    context, SearchCompany(isJobSearch: true));
+                              },
+                            ),
                           ),
-                        ),
-                  Global.userModel?.type == "user"
-                      ? ImageButton(
-                          image: MyImages.searchBlue,
-                          padding: EdgeInsets.only(left: 10, right: 20),
-                          onTap: () {
-                            AppRoutes.push(
-                                context,
-                                SearchCompany(
-                                    index: 2,
-                                    companyModel: widget.companyModel));
-                          },
-                        )
-                      : GestureDetector(
-                          onTap: () {
-                            context.read<BottomBloc>().add(SetScreenEvent(true,
-                                screenName: AddJobPositionScreen(
-                                  companyModel: widget.companyModel,
-                                )));
-                            // AppRoutes.push(context, EditListing());
-                          },
-                          child: Padding(
-                            padding:
-                                const EdgeInsets.only(right: 10.0, left: 10),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  color: MyColors.blue, shape: BoxShape.circle),
-                              child: Padding(
-                                padding: const EdgeInsets.all(9.0),
-                                child: Icon(
-                                  Icons.add,
-                                  color: MyColors.white,
+                    SizedBox(width: 5),
+                    Global.userModel?.type == "user"
+                        ? ImageButton(
+                            image: MyImages.searchBlue,
+                            padding: EdgeInsets.only(left: 10, right: 20),
+                            onTap: () {
+                              AppRoutes.push(
+                                  context,
+                                  SearchCompany(
+                                      index: 2,
+                                      companyModel: widget.companyModel));
+                            },
+                          )
+                        : GestureDetector(
+                            onTap: () {
+                              context
+                                  .read<BottomBloc>()
+                                  .add(SetScreenEvent(true,
+                                      screenName: AddJobPositionScreen(
+                                        companyModel: widget.companyModel,
+                                      )));
+                              // AppRoutes.push(context, EditListing());
+                            },
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.only(right: 10.0, left: 10),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: MyColors.blue,
+                                    shape: BoxShape.circle),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(9.0),
+                                  child: Icon(
+                                    Icons.add,
+                                    color: MyColors.white,
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        )
-                ],
-              ),
-            )),
-        body: Container(
-          child: Column(
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 15),
-                child: Container(
-                  height: 180,
-                  alignment: Alignment.bottomLeft,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: DecorationImage(
-                      image: CachedNetworkImageProvider(
-                          "${EndPoint.imageBaseUrl}${widget.companyModel?.uploadPhoto}"),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  child: Stack(
-                    children: [
-                      // Gradient overlay
-                      Container(
-                        width: double.infinity,
-                        height: double.infinity,
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter,
-                            colors: [
-                              Colors.black.withOpacity(0.6),
-                              Colors.transparent
-                            ],
-                          ),
-                        ),
-                      ),
-                      // Text content
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            // Company name
-                            CommonText(
-                              text: "${widget.companyModel?.companyName}",
-                              fontColor: MyColors.white,
-                              fontSize: 20,
-                            ),
-                            SizedBox(height: 10),
-                            // Description text
-                            CommonText(
-                              text:
-                                  "It's a long established fact that reader will directly by the",
-                              fontColor: MyColors.white,
-                              fontSize: 13,
-                            ),
-                            CommonText(
-                              text:
-                                  "readable content of page when looking at it's layout",
-                              fontColor: MyColors.white,
-                              fontSize: 13,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                          )
+                  ],
                 ),
               ),
-              SizedBox(height: 10),
-              Expanded(
-                child: BlocBuilder<JobPositionBloc, JobPosState>(
-                    builder: (context, state) {
-                  if (state is JobPosLoading) {
-                    return Center(child: CircularProgressIndicator());
-                  }
-                  if (state is JobErrorState) {
-                    return Center(child: Text(state.error));
-                  }
-                  if (state is JobPosLoaded) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 8.0, horizontal: 15),
-                      child: ListView.builder(
-                          itemCount: state.jobPosList.length,
-                          itemBuilder: (c, i) {
-                            var data = state.jobPosList[i];
-                            return JobOpeningTile(
-                                jobPosModel: data,
-                                companyModel: widget.companyModel);
-                          }),
-                    );
-                  }
-                  return SizedBox();
-                }),
-              )
-            ],
-          ),
+            )),
+        body: Column(
+          children: [
+            SizedBox(height: 12),
+            SizedBox(height: 5),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24),
+              child: Stack(
+                children: [
+                  Container(
+                    height: 180,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                        colors: [
+                          Colors.black.withOpacity(0.6),
+                          Colors.transparent
+                        ],
+                      ),
+                      image: DecorationImage(
+                        image: CachedNetworkImageProvider(
+                            "${EndPoint.imageBaseUrl}${widget.companyModel?.uploadPhoto}"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  // Text content
+                  Positioned(
+                    bottom: 18,
+                    left: 12,
+                    right: 24,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Company name
+                        CommonText(
+                          text: "${widget.companyModel?.companyName}",
+                          fontColor: MyColors.white,
+                          fontSize: 20,
+                        ),
+                        SizedBox(height: 8),
+                        // Description text
+                        CommonText(
+                          text:
+                              "It's a long established fact that reader will directly by the readable content of page when looking at it's layout",
+                          fontColor: MyColors.white,
+                          fontSize: 13,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Expanded(
+              child: BlocBuilder<JobPositionBloc, JobPosState>(
+                  builder: (context, state) {
+                if (state is JobPosLoading) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                if (state is JobErrorState) {
+                  return Center(child: Text(state.error));
+                }
+                if (state is JobPosLoaded) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 8.0, horizontal: 15),
+                    child: ListView.builder(
+                        itemCount: state.jobPosList.length,
+                        itemBuilder: (c, i) {
+                          var data = state.jobPosList[i];
+                          return JobOpeningTile(
+                              jobPosModel: data,
+                              companyModel: widget.companyModel);
+                        }),
+                  );
+                }
+                return SizedBox();
+              }),
+            )
+          ],
         ),
       ),
     );
