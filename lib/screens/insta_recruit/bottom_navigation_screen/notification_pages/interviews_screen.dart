d65e14ac0443473682907af23e124815ccaeef28
dana1 <danielaraya2191@gmail.com>
2024-04-21 15:21:00 +0300

    fix: general(all) messages section and UI error

--- lib/screens/insta_recruit/bottom_navigation_screen/notification_pages/interviews_screen.dart
+++ lib/screens/insta_recruit/bottom_navigation_screen/notification_pages/interviews_screen.dart
@@ -9,6 +9,7 @@ import 'package:insta_job/bloc/interview_schedule_cubit/interview_schedule_state
 import 'package:insta_job/bloc/job_position/job_poision_bloc.dart';
 import 'package:insta_job/bloc/job_position/job_pos_state.dart';
 import 'package:insta_job/utils/my_colors.dart';
+import 'package:insta_job/widgets/custom_cards/notifications_tile/general_message_tile.dart';
 import 'package:insta_job/widgets/custom_cards/notifications_tile/interview_tile.dart';
 import 'package:insta_job/widgets/custom_cards/notifications_tile/message_tile.dart';
 
@@ -111,55 +112,31 @@ class _InterviewScreenState extends State<InterviewScreen> {
                   ),
                   SizedBox(height: 10),
                   Expanded(
-                    child: BlocBuilder<JobPositionBloc, JobPosState>(
-                        builder: (context, state) {
-                      if (state is AppliedJobLoaded) {
-                        //TODO: run and check (URGENT)
-                        // return ListView.builder(
-                        //     shrinkWrap: true,
-                        //     itemCount: state.appliedJobList.length,
-                        //     itemBuilder: (c, i) {
-                        //       var data = state.appliedJobList[i];
-                        //       return MessageTile(
-                        //         jobPosModel: data,
-                        //       );
-                        //     });
-
-                        return StreamBuilder<Object>(
-                            stream: FirebaseFirestore.instance
-                                .collection(AuthService.chatCollection)
-                                // .where("jobId", isEqualTo: jobId)
-                                .snapshots(),
-                            builder:
-                                (context, AsyncSnapshot<dynamic> snapshot) {
-                              if (snapshot.hasData) {
-                                debugPrint(
-                                    'chats: ${snapshot.data.docs.map((e) => e.data() as Map<String, dynamic>)}');
-                                var chats =
-                                    (snapshot.data as QuerySnapshot).docs;
-                                return ListView.builder(
-                                    itemCount: chats.length,
-                                    itemBuilder: (context, index) =>
-                                        MessageTile(
-                                          jobPosModel: state.appliedJobList
-                                              .firstWhere((element) =>
-                                                  element.userFirebaseId
-                                                      ?.toString() ==
-                                                  chats[index].get('oppId')),
-                                        ));
-                              }
-                              return SizedBox();
-                            });
-                      }
-                      if (state is ApplyLoading) {
-                        Center(child: CircularProgressIndicator());
-                      }
-                      if (state is ApplyErrorState) {
-                        Center(child: Text(state.error));
-                      }
-                      return SizedBox();
-                    }),
-                  ),
+                      child: StreamBuilder<Object>(
+                          stream: FirebaseFirestore.instance
+                              .collection(AuthService.chatCollection)
+                              .snapshots(),
+                          builder: (context, AsyncSnapshot<dynamic> snapshot) {
+                            if (snapshot.hasData) {
+                              debugPrint(
+                                  'chats: ${snapshot.data.docs.map((e) => e.data() as Map<String, dynamic>)}');
+                              var chats = (snapshot.data as QuerySnapshot).docs;
+                              return ListView.builder(
+                                  itemCount: chats.length,
+                                  itemBuilder: (context, index) {
+                                    var chatItemDoc = chats[index];
+                                    Map<String, dynamic>? chatItemData =
+                                        chatItemDoc.data()
+                                            as Map<String, dynamic>?;
+                                    return GeneralMessageTile(
+                                      oppId: chatItemData?["oppId"],
+                                      selfId: chatItemData?["selfId"],
+                                    );
+                                  });
+                            }
+                            return SizedBox.shrink();
+                            // return Center(child: Text("No messages yet"));
+                          })),
                   /*SizedBox(
                     height: 250,
                     child: ListView.builder(
@@ -208,8 +185,7 @@ class _InterviewScreenState extends State<InterviewScreen> {
           ),
         ),
       );
-    }
-    );
+    });
   }
 
   Widget _buildUpcomingInterviewTabDetails() {
@@ -224,51 +200,23 @@ class _InterviewScreenState extends State<InterviewScreen> {
           right: 16,
           top: 16,
         ),
-        child: Column(
-          children: [
-            GridView.builder(
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              padding: EdgeInsets.only(bottom: 8),
-              itemCount: 4,
-              itemBuilder: (c, i) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 0),
-                  child: InterviewTile(),
-                );
-              },
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 8,
-                crossAxisSpacing: 5,
-                childAspectRatio: 3 / 7,
-              ),
-            ),
-            const SizedBox(height: 12),
-            // Center(
-            // child: DotsIndicator(
-            //   dotsCount: 5,
-            //   position: 3,
-            //   decorator: const DotsDecorator(
-            //     color: ColorUtils.darkGrey,
-            //     activeColor: ColorUtils.white,
-            //     size: Size(6, 6),
-            //     activeSize: Size(8, 8),
-            //   ),
-            // ),
-            // child: DotsIndicator(
-            //   dotsCount: 4, // Total number of pages
-            //   position: _currentPage.toDouble(),
-            //   decorator: DotsDecorator(
-            //     size: const Size.square(8.0),
-            //     activeSize: const Size(20.0, 8.0),
-            //     activeShape: RoundedRectangleBorder(
-            //       borderRadius: BorderRadius.circular(5.0),
-            //     ),
-            //   ),
-            // ),
-            // ),
-          ],
+        child: GridView.builder(
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          padding: EdgeInsets.only(bottom: 8),
+          itemCount: 4,
+          itemBuilder: (c, i) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 0),
+              child: InterviewTile(),
+            );
+          },
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 8,
+            crossAxisSpacing: 5,
+            childAspectRatio: 3 / 7,
+          ),
         ),
       );
     });
