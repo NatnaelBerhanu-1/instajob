96da6fb6d8331f7b20f349ed6d2df3b72311d3a4
dana1 <danielaraya2191@gmail.com>
2024-05-14 11:19:23 +0300

    fix: update expired static token

--- lib/utils/agora_credentials.dart
+++ lib/utils/agora_credentials.dart
@@ -1,6 +1,6 @@
 class AgoraCredentials {
   static String APP_ID = '12add1cb15ca47f68bba1d245ee91b6e';
-  static String CHANNEL_ID = 'test_token4';
+  static String CHANNEL_ID = 'testtoken5';
   static String? TOKEN =
-      "007eJxTYHhwvf/DxGcK1k+TVi5lqFlZ+fOEz7EZ6gqrq8XT/Dsrwg0VGAyNElNSDJOTDE2TE03M08wskpISDVOMTExTUy0Nk8xSp3yySWsIZGSYszaBgREKQXxuhpLU4pL4kvzs1DwTBgYA1ygkEg==";
+      "007eJxTYBCql9E/VmDF+/suk2h8yGfj4yHVMdPV8g95KT5vDpg9/6ICg6FRYkqKYXKSoWlyool5mplFUlKiYYqRiWlqqqVhkllqvJxzWkMgI4OjSjILIwMEgvhcDCWpxSUl+dmpeaYMDAB2MR+d";
 }
